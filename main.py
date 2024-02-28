0f22a2956625e92f4322a5a8e193bfc5587c5962
Elnur21 <you@example.com>
2024-02-28 10:04:50 +0100

    refactor

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from utils.helper import *
 df = read_dataset("ArrowHead")
 
 # print(len(set(df[1])))
-model = LITE("outputs",len(df[0]),len(set(df[1])))
+model = LITE("outputs",len(df[0].T),len(set(df[1])))
 
-hist = model.compile()
+model.compile()
+
+X_train = df[0].reshape(df[0].shape[0], df[0].shape[1], 1)
+X_test = df[2].reshape(df[2].shape[0], df[2].shape[1], 1)
+
+hist = model.fit(X_train,df[1],X_test,df[3])
